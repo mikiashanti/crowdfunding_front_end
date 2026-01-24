6eb79ff6678f83ba42f951c880927a0d17c7bbcc
Michaela <mgyasiagyei@gmail.com>
2026-01-24 23:51:11 +1000

    completed module, check delete button

---
+++ todo-list/src/components/TodoItem/index.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+function TodoItem(props) {
+    return (
+        <div className={`todo ${props.todo.isCompleted ? "complete" : ""}`}>
+            {props.todo.text}
+            <div>
+                <button onClick={() => props.completeTodo(props.index)}>Complete</button>
+                <button onClick={() => props.removeTodo(props.index)}>x</button>
+            </div>
+        </div>
+    )
+}
+
+export default TodoItem;
