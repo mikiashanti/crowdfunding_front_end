6a250109985625e05a1cdac1adb7d95f3dcada9f
Michaela <mgyasiagyei@gmail.com>
2026-01-31 11:15:47 +1000

    fixed error - export default useAuth

--- src/hooks/use-auth.js
+++ src/hooks/use-auth.js
@@ -6,3 +6,5 @@ export const useAuth = () => {
   // We pass in the context and create a custom hook that returns the context auth and setAuth
     return useContext(AuthContext);
 };
+
+export default useAuth
