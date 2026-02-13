434ab99cf5b1607ae4cc019d5988f8370c326ea0
Michaela <mgyasiagyei@gmail.com>
2026-02-13 21:52:28 +1000

    updated 404 page image and readme

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import adinkraImg from "../assets/img/adinkra.png";
 
 export default function NotFound() {
   return (
     <div style={{ padding: "2rem", textAlign: "center" }}>
-        <img src="../src/assets/img/adinkra.png"/>
+        <img src={adinkraImg} alt="adinkra"/>
         <p>404</p>
       <h1>Odo Nyera Fie Kwan</h1>
       <p>Love does not lose its way home</p>
